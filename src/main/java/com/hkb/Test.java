f657d6037468c95fc5ea4a1933c1528ee3168eae
huangkb <huangkb_vip@qq.com>
2020-11-18 21:59:16 +0800

    good

--- src/main/java/com/hkb/Test.java
+++ src/main/java/com/hkb/Test.java
@@ -1,14 +1,5 @@
 package com.hkb;
 
 public class Test {
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-    int a ;
-    int clone;
-    String bb="";
-    String cc="1000";
-    int c=100;
-    String dd="huangkb";
+
 }
